8dc17742bb8ca4f383e793a2433bfb9f60991d82
bodaob6 <helvisano@gmail.com>
2025-11-23 02:38:57 -0300

    Delete bot.py

--- bot.py
+++
@@ -1,7 +0,0 @@
-# opcional: permite rodar localmente com polling para testes
-import os
-from app.services.bot_handlers import run_polling
-
-if __name__ == "__main__":
-    # run_polling usa Updater/Dispatcher e faz polling (apenas para testes locais)
-    run_polling()
