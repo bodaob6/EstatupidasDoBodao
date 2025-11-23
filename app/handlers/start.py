03c8345eb9611ce7a59648f415c1b6f347241d0c
bodaob6 <helvisano@gmail.com>
2025-11-23 05:24:46 -0300

    Update start.py
    versões profissionais

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -1,7 +1,5 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "👋 Olá! O FutBodeBetBot está online!"
-    )
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Olá! Bot funcionando com webhook no Render 🚀")
